b210545d2efa3b71917dd55d277b0df9630b38ae
jpd61 <jpd@dewoodyoil.com>
2020-04-29 20:59:57 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var tmdbAPI = "24015e7692b811d33d1c989cbd42b043";
 
 var searchTerm ="casino";
 
+// GoodReads API Search using searchTerm
 //cors-anywhere fixes CORS issue - append fetch after to use
 var grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "&q=" + searchTerm;
 
@@ -13,6 +14,7 @@ $.ajax({
     console.log(response);
 });
 
+// Movie Database Search using searchTerm
 var tmdbFetch = "https://api.themoviedb.org/3/movie/550?api_key=" + tmdbAPI + "&q=" + searchTerm;
 
 $.ajax({
