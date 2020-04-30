b08fa08b10fa7f15420cc8acedddb58f0cb102fd
jpd61 <jpd@dewoodyoil.com>
2020-04-29 20:55:45 -0500

    API work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-var goodReadsAPI = "BQEOrH4cYetnk78yLa8cQA"
+var goodReadsAPI = "BQEOrH4cYetnk78yLa8cQA";
+var tmdbAPI = "24015e7692b811d33d1c989cbd42b043";
+
+var searchTerm ="casino";
 
 //cors-anywhere fixes CORS issue - append fetch after to use
-var grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "&q=Ender%27s+Game";
+var grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "fight club";
 
 $.ajax({
     url: grFetch,
+    method: "GET"
+}).done(response => {
+    console.log(response);
+});
+
+var tmdbFetch = "https://api.themoviedb.org/3/movie/550?api_key=" + tmdbAPI + searchTerm;
+
+$.ajax({
+    url: tmdbFetch,
     method: "GET"
 }).done(response => {
     console.log(response);
