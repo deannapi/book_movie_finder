70a52ab47e46cc55b034505c81464d3b9e801165
jpd61 <jpd@dewoodyoil.com>
2020-04-29 20:59:01 -0500

    added &q= to search

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var tmdbAPI = "24015e7692b811d33d1c989cbd42b043";
 var searchTerm ="casino";
 
 //cors-anywhere fixes CORS issue - append fetch after to use
-var grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "fight club";
+var grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "&q=" + searchTerm;
 
 $.ajax({
     url: grFetch,
@@ -13,7 +13,7 @@ $.ajax({
     console.log(response);
 });
 
-var tmdbFetch = "https://api.themoviedb.org/3/movie/550?api_key=" + tmdbAPI + searchTerm;
+var tmdbFetch = "https://api.themoviedb.org/3/movie/550?api_key=" + tmdbAPI + "&q=" + searchTerm;
 
 $.ajax({
     url: tmdbFetch,
