8aa7689df66017beb645078fe605ae694844c839
jpd61 <jpd@dewoodyoil.com>
2020-04-30 21:02:40 -0500

    where we are now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,21 @@
 var goodReadsAPI = "BQEOrH4cYetnk78yLa8cQA";
 var tmdbAPI = "24015e7692b811d33d1c989cbd42b043";
-var googleAPI = "AIzaSyCmTcR9EOLfB69f9zSsiXl19npDTHnIEG8";
 
 var searchTerm ="casino";
 
-// GoodReads API Search using searchTerm
-//cors-anywhere fixes CORS issue - append fetch after to use
-// var grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "&q=" + searchTerm;
+
+// Movie Database Search using searchTerm
+// var tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
 
 // $.ajax({
-//     url: grFetch,
+//     url: tmdbFetch,
 //     method: "GET"
 // }).done(response => {
 //     console.log(response);
-// });
-
-// Movie Database Search using searchTerm
-var tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
-
-$.ajax({
-    url: tmdbFetch,
-    method: "GET"
-}).done(response => {
-    console.log(response);
-})
+// })
 
 // Google GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
-var googleFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.googleapis.com/auth/books" + "?q=" +  searchTerm + "&key" + googleAPI ;
+var googleFetch = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
 
 $.ajax({
     url: googleFetch,
@@ -35,57 +24,61 @@ $.ajax({
     console.log(response);
 })
 
-// var runGoodSearch = (event => {
-//     let searchTerm = $("#search-input").val();
-//     let grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "&q=" + searchTerm;
-//     $.ajax({
-//         url: grFetch,
-//         method: 'get',
-//     }).done(response => {
-//         console.log(response.books[index]['book'][0]['title'][0]);
-//     });
-// })
+// TMDB Search
+var runTMDBSearch = (event => {
+    let searchTerm = $("#search-input").val();
+    let tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
+
+    fetch(tmdbFetch)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        console.log(response.results[0].title)
+        let renderHTML = `
+        <div id="movies"></div>`
+    
+        renderHTML += `
+        <ul >
+            <li>${response.results[0].title}</li>
+
+                        </ul>`;
+        $('#movies').html(renderHTML);
+    })
+})
+
+// Google Books Search
+var runGBSearch = (event => {
+    let searchTerm = $("#search-input").val();
+    var googleFetch = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
+
+    fetch(googleFetch)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        console.log(response.items[0].volumeInfo.title)
+        let renderHTML = `
+        <div id="books"></div>`
+    
+        renderHTML += `
+        <a href="#"><img src= "${response.items[0].volumeInfo.imageLinks.smallThumbnail}"></a>
+        <ul >
+            <li>${response.items[0].volumeInfo.title}</li>
+            <li>${response.items[0].volumeInfo.authors}</li>
+            <li>${response.items[0].volumeInfo.description}</li>
+            <li><a href="${response.items[0].volumeInfo.previewLink}">Google Books Preview</a></li>
+                        </ul>`;
+        $('#books').html(renderHTML);
+    });
 
 
-// $(".button").on('click', (event) => {
-//     event.preventDefault();
-//     runGoodSearch();
-// });
-
-// var runGoodSearch = (event => {
-//     let searchTerm = $("#search-input").val();
-//     let grFetch = "https://cors-anywhere.herokuapp.com/" + "https://www.goodreads.com/search/index.xml?key=" + goodReadsAPI + "&q=" + searchTerm;
-
-//     fetch(grFetch)
-
-//     .then((response) => {
-//         console.log(response)
-//     })
-// })
+})
 
 
-// $(".button").on('click', (event) => {
-//     event.preventDefault();
-//     runGoodSearch();
-// });
 
-
-// This Works!
-// var runTMDBSearch = (event => {
-//     let searchTerm = $("#search-input").val();
-//     let tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
-
-//     fetch(tmdbFetch)
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .then((response) => {
-//         console.log(response.results[0].title)
-//     })
-// })
-
-
-// $(".button").on('click', (event) => {
-//     event.preventDefault();
-//     runTMDBSearch();
-// });
+$(".button").on('click', (event) => {
+    event.preventDefault();
+    runGBSearch();
+    runTMDBSearch();
+});
