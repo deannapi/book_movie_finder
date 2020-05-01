a24fc82ded368adcfc8ed95b6697f200e4c2892c
jpd61 <jpd@dewoodyoil.com>
2020-04-30 22:56:22 -0500

    figured out how to style and call multiple APIs in

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,14 @@ var searchTerm ="casino";
 
 
 // Movie Database Search using searchTerm
-// var tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
+var tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
 
-// $.ajax({
-//     url: tmdbFetch,
-//     method: "GET"
-// }).done(response => {
-//     console.log(response);
-// })
+$.ajax({
+    url: tmdbFetch,
+    method: "GET"
+}).done(response => {
+    console.log(response);
+})
 
 // Google GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
 var googleFetch = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
@@ -22,29 +22,6 @@ $.ajax({
     method: "GET"
 }).done(response => {
     console.log(response);
-})
-
-// TMDB Search
-var runTMDBSearch = (event => {
-    let searchTerm = $("#search-input").val();
-    let tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
-
-    fetch(tmdbFetch)
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        console.log("movie: " + response.results[0].title)
-        let renderHTML = `
-        <div id="movies"></div>`
-    
-        renderHTML += `
-        <ul >
-            <li>${response.results[0].title}</li>
-
-                        </ul>`;
-        $('#movies').html(renderHTML);
-    })
 })
 
 // Google Books Search
@@ -58,21 +35,29 @@ var runGBSearch = (event => {
     })
     .then((response) => {
         console.log("book: " + response.items[0].volumeInfo.title)
-        let renderHTML = `
-        <div id="books"></div>`
-    
-        renderHTML += `
-        <a href="#"><img src= "${response.items[0].volumeInfo.imageLinks.smallThumbnail}"></a>
-        <ul >
-            <li>${response.items[0].volumeInfo.title}</li>
-            <li>${response.items[0].volumeInfo.authors}</li>
-            <li>${response.items[0].volumeInfo.description}</li>
-            <li><a href="${response.items[0].volumeInfo.previewLink}">Google Books Preview</a></li>
-                        </ul>`;
-        $('#books').html(renderHTML);
+        $('#menu-title').html(response.items[0].volumeInfo.title);
+        $('#image').html(`<a href="#"><img src="${response.items[0].volumeInfo.imageLinks.smallThumbnail}"></a>`);
+        $('#title').html(response.items[0].volumeInfo.title);
+        $('#author').html(response.items[0].volumeInfo.authors);
+        $('#google-preview').html(`<a href="${response.items[0].volumeInfo.previewLink}">Google Books Preview</a>`);
+        $('#book-description').html("<h5>Book Description: </h5>" + response.items[0].volumeInfo.description + "<br>");
+
     });
+})
 
+// MOVIE TMDB Search
+var runTMDBSearch = (event => {
+    let searchTerm = $("#search-input").val();
+    let tmdbFetch = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPI + "&query=" + searchTerm;
 
+    fetch(tmdbFetch)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        console.log("movie: " + response.results[0].title)
+        $('#movie-description').html("<h5>Movie Description: </h5>" + response.results[0].overview);
+    })
 })
 
 
